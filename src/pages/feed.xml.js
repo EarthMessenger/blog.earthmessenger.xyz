c6314b1ffe2a3586b743a69d9735f558471d4707
earthmessenger <me@earthmessenger.xyz>
2023-07-29 18:35:15 +0800

    fix: 修复错误的 RSS 排序

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,13 +1,13 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_LANG, SITE_DISCRIPTION } from "../config";
 import sanitizeHtml from "sanitize-html";
 
 export async function get() {
   const items = import.meta.glob("./posts/*.md", { eager: true });
   const posts = Object.values(items)
-    .sort((a, b) => Date.parse(b.pubDate) - Date.parse(a.pubDate))
+    .sort((a, b) => Date.parse(b.frontmatter.pubDate) - Date.parse(a.frontmatter.pubDate))
     .slice(0, 10);
-    return rss({
+  return rss({
     title: SITE_TITLE,
     description: SITE_DISCRIPTION,
     site: "https://blog.earthmessenger.xyz",
