3015ea216d17f0d59e74bb0d9bc14694cfb3c132
earthmessenger <me@earthmessenger.xyz>
2023-11-14 19:54:55 +0800

    dep: update dependencies
    更新了 @astrojs/rss @astrojs/sitemap astro giscus rehype-mathjax remark-join-cjk-lines sanitize-html
    remark-math 暂未更新：https://github.com/remarkjs/remark-math/issues/89#issuecomment-1795190746
    
    fix: 将 feed.xml.js 中 get 重命名为 GET

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -2,7 +2,7 @@ import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_LANG, SITE_DISCRIPTION } from "../config";
 import sanitizeHtml from "sanitize-html";
 
-export async function get() {
+export async function GET() {
   const items = import.meta.glob("./posts/*.md", { eager: true });
   const posts = Object.values(items)
     .sort((a, b) => Date.parse(b.frontmatter.pubDate) - Date.parse(a.frontmatter.pubDate))
